917ea0baa7779a107d226356d19971124d01fc3e
krishna chaitanya ashish  vattem <76241544+Ashishvkc@users.noreply.github.com>
2021-08-06 11:43:49 -0400

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,9 +1,7 @@
 echo 'Starting to Deploy...'
-ssh ubuntu@3.234.99.37 " sudo docker image prune -f 
-        cd /newstorage/users/sujay/R_check 
+ssh ubuntu@3.234.99.37 " 
+        cd /newstorage/users/ashish/R_check 
         sudo docker-compose down
         git fetch origin
-        git reset --hard origin  &&  echo 'You are doing well'
-        sudo docker-compose build && sudo docker-compose up -d
         "
 echo 'Deployment completed successfully'
