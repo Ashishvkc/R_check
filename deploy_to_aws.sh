54f8012618379c53dcca485246012d271252dea0
krishna chaitanya ashish  vattem <76241544+Ashishvkc@users.noreply.github.com>
2021-08-06 12:30:29 -0400

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -3,6 +3,7 @@ ssh ubuntu@3.234.99.37 "
         cd /newstorage/users/ashish/R_check 
         mkdir ad_temp
         echo "created temp"
+        git pull
         git fetch origin
         "
 echo 'Deployment completed successfully'
