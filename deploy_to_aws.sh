c88f76d7a87acd24e17b8800221368de3c8158ef
krishna chaitanya ashish  vattem <76241544+Ashishvkc@users.noreply.github.com>
2021-08-06 15:44:21 -0400

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -1,8 +1,7 @@
 echo 'Starting to Deploy...'
 ssh ubuntu@3.234.99.37 " 
         sudo su -l ashish 
-        cd ~/R_check 
-        mkdir ash
+        cd ~/R_check
        
         git push
         
