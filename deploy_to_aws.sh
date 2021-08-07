a95e11d86b7b3f0f5511ea8a585720703e538344
krishna chaitanya ashish  vattem <76241544+Ashishvkc@users.noreply.github.com>
2021-08-07 19:59:01 -0400

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -6,7 +6,7 @@ ssh ubuntu@3.234.99.37 "
         git fetch
         echo "Fetched"
         git pull
-        echo "pulled"
+        echo "pulledsd"
         
         "
 echo 'Deployment completed successfully'
