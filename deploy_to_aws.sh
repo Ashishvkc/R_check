34dd8aefff752dc1e2ed5968c736599d23b67726
krishna chaitanya ashish  vattem <76241544+Ashishvkc@users.noreply.github.com>
2021-08-07 16:27:47 -0400

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -5,8 +5,8 @@ ssh ubuntu@3.234.99.37 "
         cd ~/R_check
         git fetch
         echo "Fetched"
-        git push https://github.com/Ashishvkc/R_check.git
-        echo "Pushed"
+        git pull
+        echo "pulled"
         
         "
 echo 'Deployment completed successfully'
