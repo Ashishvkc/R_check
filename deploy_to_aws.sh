1adcc2aa6ac5c41a3eb551a13140d296850a7dfa
krishna chaitanya ashish  vattem <76241544+Ashishvkc@users.noreply.github.com>
2021-08-07 20:06:52 -0400

    Update deploy_to_aws.sh

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -6,7 +6,7 @@ ssh ubuntu@3.234.99.37 "
         git fetch
         echo "Fetched"
         
-        git clone git@github.com:Ashishvkc/R_check.git
+        git pull
         echo "cloned"
         
         "
